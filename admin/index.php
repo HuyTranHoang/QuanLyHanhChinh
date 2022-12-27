583d98fac2af18374d27a895a249e6aff577b8b7
Huy <huyth2520@gmail.com>
2022-12-28 01:45:48 +0700

    Xóa module header footer, thêm check login admin

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script defer src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Admin</title>
+</head>
+
+<body>
+<nav class="navbar navbar-dark bg-secondary container-fluid">
+    <div class="container position-relative p-2">
+        <a class="navbar-brand" href="#">
+            <img src="../images/logo.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+            Aptech - C2206L - Huy Nè
+        </a>
+        <h1 class="navbar-text position-absolute top-50 start-50 translate-middle text-white">Quản Lý Hành Chính</h1>
+    </div>
+</nav>
+
+
 <?php
 
+session_start();
+if (isset($_SESSION['role']) && $_SESSION['role'] == '1') {
     include_once '../model/db.php';
     include_once '../model/phongban.php';
     connectDB();
-    include  'view/header.php';
+
 
     if (isset($_GET['act'])) {
         switch ($_GET['act']) {
@@ -12,7 +40,7 @@
                 include 'view/phongban.php';
                 break;
             case 'delpb':
-                if(isset($_GET['id'])) {
+                if (isset($_GET['id'])) {
                     $id = $_GET['id'];
                     delpb($id);
                     $kq = getAll_pb();
@@ -32,22 +60,27 @@
                 include 'view/phieunghiphep.php';
                 break;
             case 'thoat':
-                include 'view/thoat.php';
+                if (isset($_SESSION['role'])) {
+                    unset($_SESSION['role']);
+                }
+                header('location:../login.php');
                 break;
             default:
-                include  'view/home.php';
+                include 'view/home.php';
                 break;
         }
     } else {
-        include  'view/home.php';
+        include 'view/home.php';
     }
+} else {
+    header('location: ../login.php');
+}
 
-    include 'view/footer.php';
+
 ?>
+</body>
+</html>
 
-
-<!---->
-<!---->
 <?php
 //    if (isset($_POST['userName']) && isset($_POST['password'])) {
 //        $conn = connectDB();
@@ -82,4 +115,5 @@
 //
 //    echo '<h1 class="text-primary">Welcome back '.$user['tenNV'].'</h1>'
 //
-//?>
+
+?>
---
+++ model/user.php
@@ -0,0 +1,43 @@
+<?php
+
+//function checkUser($user,$pass) {
+//    $conn = connectDB();
+//    $stmt = $conn->prepare("SELECT * FROM nhanvien WHERE ? and ?");
+//    $pre = $conn->prepare($stmt);
+//    $pre->bind_param("ss",$userName,$password);
+//    $stmt->execute();
+//    $t = $pre->execute();
+//    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+//    $kq = $stmt->fetchAll();
+//    return $kq[0]['maCV'];
+//}
+function checkUser($user, $pass) {
+    $conn = connectDB();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM nhanvien WHERE userName=:username AND password=:password");
+    $stmt->bindParam(":username", $user);
+    $stmt->bindParam(":password", $pass);
+    $stmt->execute();
+    $kq = $stmt->fetch(PDO::FETCH_ASSOC);
+    return (empty($kq)) ? null : $kq['maCV'];
+
+    // Lấy nhiều dòng
+//    $kq = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//    if ($kq === false) {
+//        // No rows were returned, or there was an error executing the query
+//        // You can handle the error here, or return a default value
+//        return null;
+//    } else {
+//        return $kq[0]['maCV'];
+//    }
+
+    // Lấy một dòng
+//    $kq = $stmt->fetch(PDO::FETCH_ASSOC);
+//    if ($kq === false || $kq === null) {
+//        // No rows were returned, or there was an error executing the query
+//        // You can handle the error here, or return a default value
+//        return null;
+//    } else {
+//        return $kq['maCV'];
+//    }
+}
