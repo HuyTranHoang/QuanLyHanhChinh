d0f533a3ac4f56a5519100a0c5eb1ca43e8b3de9
Huy <huyth2520@gmail.com>
2022-12-29 02:18:05 +0700

    aaa

--- admin/view/footer.php
+++ admin/view/footer.php
@@ -1,3 +1,4 @@
-<div class="col offset-2">
+<?php
+echo '<div class="col offset-2">
     <h1 class="">FOOTER PLACE HOLDER</h1>
-</div>
+</div>';
--- admin/view/header.php
+++ admin/view/header.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-dark bg-secondary container-fluid">
+<?php
+echo '<nav class="navbar navbar-dark bg-secondary container-fluid">
     <div class="container position-relative p-2 offset-2">
         <a class="navbar-brand" href="#" style="color: #D2E4F1">
             <!--            <img src="../images/logo.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">-->
@@ -7,4 +8,4 @@
         </a>
         <h1 class="navbar-text position-absolute top-50 start-50 translate-middle text-white">Quản Lý Hành Chính</h1>
     </div>
-</nav>
+</nav>';
