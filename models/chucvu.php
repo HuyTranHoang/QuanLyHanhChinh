9e13b7e7bd1a0bb731c7c07ec60e6b5868554f4d
Huy <huyth2520@gmail.com>
2022-12-29 18:21:34 +0700

    Chuyển chức vụ thành OOP

--- models/chucvu.php
+++ models/chucvu.php
@@ -1,45 +1,61 @@
 <?php
-
-function insertCV ($chucVu) {
-    $conn = connectDB();
-    $stmt = $conn->prepare("INSERT INTO chucvu (maCV, chucVu) VALUES (NULL,:chucVu)");
-    $stmt->bindParam(":chucVu", $chucVu);
-    $stmt->execute();
-}
-
-function updateCV ($id, $chucVu)
+class chucVu
 {
-    $conn = connectDB();
-    $sql = "UPDATE chucvu SET chucVu=:chucVu WHERE maCV=:id";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(":id", $id);
-    $stmt->bindParam(":chucVu", $chucVu);
-    $stmt->execute();
-}
+    public $conn;
 
-function getOne_CV($id)
-{
-    $conn = connectDB();
-    $stmt = $conn->prepare("SELECT * FROM chucvu where maCV =" . $id);
-    $stmt->execute();
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $kq = $stmt->fetch();
-    return $kq;
-}
+    public function __construct()
+    {
+    }
 
-function delCV($id)
-{
-    $conn = connectDB();
-    $sql = "DELETE FROM chucvu WHERE maCV=" . $id;
-    $conn->exec($sql);
-}
+    public function insertPB($chucVu)
+    {
+        $db = new db();
+        $this->conn = $db->connectDB();
+        $query = "INSERT INTO chucvu (maCV, chucVu) VALUES (NULL,:chucVu)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":chucVu", $chucVu);
+        $stmt->execute();
+    }
 
-function getAll_cv()
-{
-    $conn = connectDB();
-    $stmt = $conn->prepare("SELECT * FROM chucvu");
-    $stmt->execute();
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $kq = $stmt->fetchAll();
-    return $kq;
+    public function updateCV($id, $chucVu)
+    {
+        $db = new db();
+        $this->conn = $db->connectDB();
+        $query = "UPDATE chucvu SET chucVu=:chucVu WHERE maCV=:id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":chucVu", $chucVu);
+        $stmt->execute();
+    }
+
+    public function getOne_CV($id)
+    {
+        $db = new db();
+        $this->conn = $db->connectDB();
+        $query = "SELECT * FROM chucvu where maCV =" . $id;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        return $stmt->fetch();
+    }
+
+    public function delCV($id)
+    {
+        $db = new db();
+        $this->conn = $db->connectDB();
+        $query = "DELETE FROM chucvu WHERE maCV=" . $id;
+        $this->conn->exec($query);
+    }
+
+    public function getAll_CV()
+    {
+        $db = new db();
+        $this->conn = $db->connectDB();
+        $query = "SELECT * FROM chucvu";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll();
+    }
+
 }
