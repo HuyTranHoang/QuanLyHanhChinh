40370ce28c70c46c1535e12d1d51ab0cbda8501e
Tran Hoang Huy <nhokpio95@gmail.com>
2023-01-14 01:38:57 +0700

    123

--- admin/page_request.php
+++ admin/page_request.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
-    <title>Ajax example 456</title>
+    <title>Ajax example 123</title>
 </head>
 <body>
 
